81d9c0e8fc02f0b4bfba8e75f0fdb00e0d9a6700
Juan Roa <jroar26.git@gmail.com>
2024-09-27 20:46:09 -0500

    data base connection

---
+++ ecommerce-project/ecommerce-backend/src/data-source.ts
@@ -0,0 +1,13 @@
+// src/data-source.ts
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '123456',
+  database: 'ecommerce',
+  synchronize: true,
+  entities: [__dirname + '/entities/*.ts'],
+});
