6ad76769e098d74024598d824872f76960b8046f
Juan Roa <jroar26.git@gmail.com>
2024-09-29 15:24:46 -0500

    add condition to improve the test and don't start server

--- ecommerce-project/ecommerce-backend/src/index.ts
+++ ecommerce-project/ecommerce-backend/src/index.ts
@@ -16,9 +16,14 @@ app.use('/api', OrderRoutes);
 // Inicializar conexión a la base de datos y luego iniciar el servidor
 AppDataSource.initialize()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    console.log('Database connected successfully');
+
+    // Solo iniciar el servidor si no estamos en modo de prueba
+    if (process.env.NODE_ENV !== 'test') {
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    }
   })
   .catch((error) => console.log('Error initializing database', error));
 
