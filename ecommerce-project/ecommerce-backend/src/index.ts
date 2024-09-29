b92413741a01e8e3fc303924feff83ab66874333
Juan Roa <jroar26.git@gmail.com>
2024-09-28 21:52:46 -0500

    export app to use it in tests

--- ecommerce-project/ecommerce-backend/src/index.ts
+++ ecommerce-project/ecommerce-backend/src/index.ts
@@ -3,7 +3,7 @@ import { AppDataSource } from './database';
 import ProductRoutes from './routes/ProductRoutes';
 import OrderRoutes from './routes/OrderRoutes';
 
-export const app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
@@ -13,10 +13,13 @@ app.use(express.json());
 app.use('/api', ProductRoutes);
 app.use('/api', OrderRoutes);
 
-// Iniciar Servidor
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Inicializar conexión a la base de datos y luego iniciar el servidor
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log('Error initializing database', error));
 
-// Inicializar conexión a la base de datos
-AppDataSource.initialize();
+export { app };
