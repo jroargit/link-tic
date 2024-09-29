04c48a577f0fbb37cfea2bdba935a82ec5c8d3b6
Juan Roa <jroar26.git@gmail.com>
2024-09-29 17:07:34 -0500

    change configuration to connect db in web service Render

--- ecommerce-project/ecommerce-backend/src/database/index.ts
+++ ecommerce-project/ecommerce-backend/src/database/index.ts
@@ -8,13 +8,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: [Product, Order],
   synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 AppDataSource.initialize()
